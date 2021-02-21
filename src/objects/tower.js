629da6b33f00636176bb63140cdd2ebd465fd760
santiagofontana7 <santiagofontana7@gmail.com>
2021-02-21 13:21:03 -0300

    -

--- src/objects/tower.js
+++ src/objects/tower.js
@@ -4,33 +4,31 @@ var Tower = new Phaser.Class({
 
     initialize:
 
-    function Tower (scene)
-    {
-        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'spritesBase', 'tower');
-        this.nextTic = 0;
-    },
-    place: function(i, j) {            
-        this.y = i ;
-        this.x = j ;
+        function Tower(scene) {
+            Phaser.GameObjects.Image.call(this, scene, 0, 0, 'spritesBase', 'tower');
+            this.nextTic = 0;
+        },
+    place: function (i, j) {
+        this.y = i;
+        this.x = j;
         this.setActive(true);
-        this.setVisible(true);     
-        this.setScale(0.3);  
+        this.setVisible(true);
+        this.setScale(0.3);
     },
-    fire: function() {
-        if(plane.scene)
-        {
-            var angle = Phaser.Math.Angle.Between(this.x, this.y, plane.x, plane.y);
-            if(Phaser.Math.Distance.Between(this.x, this.y, plane.x, plane.y) < 200)
-            {
-                addBulletTorret(this.x, this.y, angle);
+    fire: function () {
+        if (plane != null) {
+            if (plane.scene) {
+                var angle = Phaser.Math.Angle.Between(this.x, this.y, plane.x, plane.y);
+                if (Phaser.Math.Distance.Between(this.x, this.y, plane.x, plane.y) < 200) {
+                    addBulletTorret(this.x, this.y, angle);
+                }
+
+                //this.angle = (angle + Math.PI/2) * Phaser.Math.RAD_TO_DEG;
             }
-            
-            //this.angle = (angle + Math.PI/2) * Phaser.Math.RAD_TO_DEG;
         }
     },
-    update: function (time, delta)
-    {
-        if(time > this.nextTic) {
+    update: function (time, delta) {
+        if (time > this.nextTic) {
             this.fire();
             this.nextTic = time + 1500;
         }
--- src/objects/turret.js
+++ src/objects/turret.js
@@ -4,33 +4,31 @@ var Turret = new Phaser.Class({
 
     initialize:
 
-    function Turret (scene)
-    {
-        Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'turret');
-        this.nextTic = 0;
-    },
-    place: function(i, j) {            
-        this.y = i ;
-        this.x = j ;
+        function Turret(scene) {
+            Phaser.GameObjects.Image.call(this, scene, 0, 0, 'sprites', 'turret');
+            this.nextTic = 0;
+        },
+    place: function (i, j) {
+        this.y = i;
+        this.x = j;
         this.setActive(true);
         this.setVisible(true);
-        this.setScale(0.5);       
+        this.setScale(0.5);
     },
-    fire: function() {
-        if(plane.scene)
-        {
-            var angle = Phaser.Math.Angle.Between(this.x, this.y, plane.x, plane.y);
-            if(Phaser.Math.Distance.Between(this.x, this.y, plane.x, plane.y) < 200)
-            {
-                addBulletTorret(this.x, this.y, angle);
+    fire: function () {
+        if (plane != null) {
+            if (plane.scene) {
+                var angle = Phaser.Math.Angle.Between(this.x, this.y, plane.x, plane.y);
+                if (Phaser.Math.Distance.Between(this.x, this.y, plane.x, plane.y) < 200) {
+                    addBulletTorret(this.x, this.y, angle);
+                }
+
+                this.angle = (angle + Math.PI / 2) * Phaser.Math.RAD_TO_DEG;
             }
-            
-            this.angle = (angle + Math.PI/2) * Phaser.Math.RAD_TO_DEG;
         }
     },
-    update: function (time, delta)
-    {
-        if(time > this.nextTic) {
+    update: function (time, delta) {
+        if (time > this.nextTic) {
             this.fire();
             this.nextTic = time + 1500;
         }
@@ -42,12 +40,11 @@ function canPlaceTurret(i, j) {
     return true;
 }
 
-function placeTurret(i,j) {
-    if(canPlaceTurret(i, j)) {
+function placeTurret(i, j) {
+    if (canPlaceTurret(i, j)) {
         var turret = turrets.get();
-        if (turret)
-        {
+        if (turret) {
             turret.place(i, j);
-        }   
+        }
     }
 }
